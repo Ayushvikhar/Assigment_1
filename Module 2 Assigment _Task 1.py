cd9c1086670fdc2fa4b25afbcf3642bcef8141b1
Ayush :) <ayushvikhar123@gmail.com>
2025-07-09 14:17:24 +0530

    python file added

---
+++ Module 2 Assigment _Task 1.py
@@ -0,0 +1,9 @@
+#Basic mathematical operation
+a = input('enter a number for operation : ')
+b = input('enter another number for operation : ')
+a = float(a)
+b = float(b)
+print('addition: ',a + b)
+print('Subtraction: ',a - b)
+print('Multiplication: ',a * b)
+print('Division: ',a / b)
